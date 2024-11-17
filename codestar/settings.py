8a984c0f6bdbe0119846b26b629ad6ee733de2ea
Ellie Epanswick <espanswick@gmail.com>
2024-11-17 20:19:46 +0000

    Add comment model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-els390-cidjangoblog-en7kmmrrt84.ws.codeinstitute-ide.net','.herokuapp.com',]
 
