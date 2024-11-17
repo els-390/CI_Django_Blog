faa7ecb2bc0e5a2f15d587f8b87d5aee16c5e391
Ellie Epanswick <espanswick@gmail.com>
2024-11-17 21:24:41 +0000

    Add meta class and str model to posts model

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,11 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"The title of this post is {self.title}"
+
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
